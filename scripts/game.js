25d97ed038e39b1c5dc5e28cd909f90c28eebe9c
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-06-06 23:36:43 +0530

    Basic Snake Game Program

---
+++ scripts/game.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, Snake_Speed, getSnakeHead, snakeIntersection } from './snake.js';
+import { update as updateFood, draw as drawFood} from './food.js'
+import { outsideGrid } from './grid.js'
+/* Gameloop is a function  which repeats itself over and over again on a set interval that way we can constantly update our render */
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime) {
+    if(gameOver) {
+        if(confirm('You lost. Press OK to restart.')) {
+            window.location = '/';
+        }
+        return;
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceRender = (currentTime - lastRenderTime)/1000;
+    if(secondsSinceRender < 1 / Snake_Speed) return
+    
+
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+function draw() {
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
